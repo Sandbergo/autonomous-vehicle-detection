7079e87b0b2ec1651e65a2f3b3f5471a5292458d
Olav Landmark Pedersen <olavpe@gmail.com>
2020-04-26 23:51:27 +0200

    working on effnet2 and trying with 920x720

--- SSD/ssd/modeling/backbone/resnet.py
+++ SSD/ssd/modeling/backbone/resnet.py
@@ -17,50 +17,29 @@ class ResNet(nn.Module):
         # https://github.com/NVIDIA/DeepLearningExamples/blob/master/PyTorch/Detection/SSD/src/model.py
         # extra block implementation taken from
         # https://github.com/pytorch/vision/blob/7b60f4db9707d7afdbb87fd4e8ef6906ca014720/torchvision/models/resnet.py#L35
-        
+
         resnet = models.resnet34(pretrained=cfg.MODEL.BACKBONE.PRETRAINED)
-       
+
         self.resnet = nn.Sequential(*list(resnet.children())[:8])
-        
-        self.resnet[3] = nn.MaxPool2d(kernel_size=1, stride=1, padding=0) # hukk old 
-        
+
+        self.resnet[3] = nn.MaxPool2d(kernel_size=1, stride=1, padding=0) # hukk old
+
         conv4_block1 = self.resnet[-1][0]
         conv4_block1.conv1.stride = (1, 1)
         conv4_block1.conv2.stride = (1, 1)
         conv4_block1.downsample[0].stride = (1, 1)
 
-        self.resnet = nn.Sequential(*list(self.resnet.children()), 
-                    BasicBlock(inplanes = 512, planes = 512, stride=2)) # BasicBlock(inplanes = 512, planes = 512, stride=2),
-        
+        self.resnet = nn.Sequential(*list(self.resnet.children()),
+                    BeginBlock(inplanes = 512, planes = 512, stride=3)) # BasicBlock(inplanes = 512, planes = 512, stride=2),
+
         self.additional_layers = self.add_additional_layers()
         print(self.resnet)
-        
-        """
-        resnet.relu = nn.LeakyReLU(inplace=True)
-        resnet.layer1[0].relu = nn.LeakyReLU(inplace=True)
-        resnet.layer1[1].relu = nn.LeakyReLU(inplace=True)
-        resnet.layer1[2].relu = nn.LeakyReLU(inplace=True)
-        resnet.layer2[0].relu = nn.LeakyReLU(inplace=True)
-        resnet.layer2[1].relu = nn.LeakyReLU(inplace=True)
-        resnet.layer2[2].relu = nn.LeakyReLU(inplace=True)
-        resnet.layer2[3].relu = nn.LeakyReLU(inplace=True)
-        resnet.layer3[0].relu = nn.LeakyReLU(inplace=True)
-        resnet.layer3[1].relu = nn.LeakyReLU(inplace=True)
-        resnet.layer3[2].relu = nn.LeakyReLU(inplace=True)
-        resnet.layer3[3].relu = nn.LeakyReLU(inplace=True)
-        resnet.layer3[4].relu = nn.LeakyReLU(inplace=True)
-        resnet.layer3[5].relu = nn.LeakyReLU(inplace=True)
-        resnet.layer4[0].relu = nn.LeakyReLU(inplace=True)
-        resnet.layer4[1].relu = nn.LeakyReLU(inplace=True)
-        resnet.layer4[2].relu = nn.LeakyReLU(inplace=True)
-        """
 
         return
 
     def add_additional_layers(self):
         layers = nn.ModuleList()
 
-        
         for i in range(len(self.output_feature_size) - 2):
             layers.append(nn.Sequential(
                  BasicBlock(inplanes = self.output_channels[i], planes = self.output_channels[i+1], stride=2)
@@ -68,7 +47,7 @@ class ResNet(nn.Module):
         layers.append(nn.Sequential(
             EndBlock(inplanes = self.output_channels[i+1], planes = self.output_channels[i+2], stride=2)
         ))
-        
+
 
         return layers
 
@@ -94,7 +73,7 @@ class ResNet(nn.Module):
             expected_shape = feature_map_size_list[idx]
             assert feature.shape[1:] == expected_shape, \
                 f"Expected shape: {expected_shape}, got: {feature.shape[1:]} at output IDX: {idx}"
-        
+
         return tuple(features)
 
 
@@ -115,7 +94,7 @@ class BasicBlock(nn.Module):
     def __init__(self, inplanes, planes, stride=1, downsample=None, groups=1,
                  base_width=64, dilation=1, norm_layer=nn.BatchNorm2d):
         super(BasicBlock, self).__init__()
-        
+
         downsample = nn.Sequential(
             conv1x1(inplanes, planes, stride),
             norm_layer(planes),
@@ -141,11 +120,58 @@ class BasicBlock(nn.Module):
         out = self.conv2(out)
         out = self.bn2(out)
 
-        
+
         out = self.elu(out)
         out = self.conv3(out)
         out = self.bn3(out)
-        
+
+        if self.downsample is not None:
+            identity = self.downsample(x)
+
+        out += identity
+        out = self.elu(out)
+
+        return out
+
+class BeginBlock(nn.Module):
+    expansion = 1
+
+    def __init__(self, inplanes, planes, stride=1, downsample=None, groups=1,
+                 base_width=64, dilation=1, norm_layer=nn.BatchNorm2d):
+        super(BasicBlock, self).__init__()
+
+        downsample = nn.Sequential(
+            conv1x1(inplanes, planes, stride),
+            norm_layer(planes),
+        )
+
+        # self.conv1 = conv3x3(inplanes, planes, stride)
+        self.conv1 = nn.Conv2d(self.inplanes, self.planes, stride=3, padding=1, kernal_size=5)
+        # self.conv3 = nn.Conv2d(self.inplanes, self.planes, kernel_size=(2,3), stride=2, padding=0)
+        self.bn1 = norm_layer(planes)
+        self.elu = nn.ELU(inplace=True)
+        self.conv2 = conv3x3(planes, planes)
+        self.bn2 = norm_layer(planes)
+        self.conv3 = conv3x3(planes, planes)
+        self.bn3 = norm_layer(planes)
+        self.downsample = downsample
+        self.stride = stride
+
+    def forward(self, x):
+        identity = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.elu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+
+
+        out = self.elu(out)
+        out = self.conv3(out)
+        out = self.bn3(out)
+
         if self.downsample is not None:
             identity = self.downsample(x)
 
@@ -160,7 +186,7 @@ class EndBlock(nn.Module):
     def __init__(self, inplanes, planes, stride=1, downsample=None, groups=1,
                  base_width=64, dilation=1, norm_layer=nn.BatchNorm2d):
         super(EndBlock, self).__init__()
-        
+
         downsample = nn.Sequential(
             conv1x1(inplanes, planes, stride),
             norm_layer(planes),
@@ -189,7 +215,7 @@ class EndBlock(nn.Module):
         out = self.bn2(out)
         out = self.elu(out)
         out = self.conv3(out)
-        
+
         if self.downsample is not None:
             identity = self.downsample(identity)
 
@@ -202,5 +228,5 @@ class EndBlock(nn.Module):
 @registry.BACKBONES.register('resnet')
 def resnet(cfg, pretrained=True):
     model = ResNet(cfg)
-    
+
     return model
